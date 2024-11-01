07e1e5a9c1a9f9cf9bb949ffc10595813fac6233
Alistair Grant <alistairgrant@fastmail.com.au>
2024-11-01 15:56:01 +0100

    Add GemStone Rowan3 installation scripts
    Not Yet Working

---
+++ scripts/installGToolkitWireEncoding_rowan3.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+### Install gtoolkit-wireencoding from Tonel files into a Rowan-enabled stone
+### Exits with 0 if success, 1 if failed
+
+gt4GemstoneHome=${ROWAN_PROJECTS_HOME}/gtoolkit-wireencoding
+## Topaz refuses to exit from script if input is stdin, so redirect from /dev/zero
+topaz -l -I ${gt4GemstoneHome}/scripts/loginSystemUser.topaz  -S ${gt4GemstoneHome}/scripts/installGtoolkit-wireencoding.topaz < /dev/zero
+if [ $? = 0 ]
+    then
+        echo gtoolkit-wireencoding loaded
+    else
+        echo !!!!!!!!!!!!!!
+        echo INSTALL FAILED for gtoolkit-wireencoding
+        echo !!!!!!!!!!!!!!
+        exit 1
+    fi
+
